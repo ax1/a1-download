d62fa87f283a901754b04c94996881868f5db740
Angel Rego <angelrego@gmail.com>
2019-08-18 21:32:05 +0200

    compile from ts to js

--- index.js
+++ index.js
@@ -5,19 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const node_fetch_1 = __importDefault(require("node-fetch"));
 const fs_1 = __importDefault(require("fs"));
-async function download(url, folder) {
-    try {
-        const res = await node_fetch_1.default(url);
-        if (!res.ok)
-            throw new Error(res.statusText + '. ' + res.url);
-        const folder = `./database`;
-        if (!fs_1.default.existsSync(folder))
-            fs_1.default.mkdirSync(folder, { recursive: true }); // synchronous, but only the first time
-        const file = fs_1.default.createWriteStream(`${folder}/${(new Date()).toISOString()}.html`);
-        res.body.pipe(file);
-    }
-    catch (err) {
-        console.error(err);
-    }
+async function download(url, folder, name) {
+    const res = await node_fetch_1.default(url);
+    if (!res.ok)
+        throw new Error(res.statusText + '. ' + res.url);
+    if (!folder)
+        folder = '.';
+    if (!fs_1.default.existsSync(folder))
+        fs_1.default.mkdirSync(folder, { recursive: true }); // synchronous, but only the first time
+    if (!name)
+        name = url.substring(url.lastIndexOf('/') + 1);
+    const file = fs_1.default.createWriteStream(`${folder}/${name}`);
+    res.body.pipe(file);
 }
 exports.download = download;
